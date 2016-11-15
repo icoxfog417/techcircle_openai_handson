d10fa200bc75bc9d5d9cb520b1f910103384c6c1
icoxfog417 <icoxfog417@yahoo.co.jp>
2016-11-15 16:38:52 +0900

    arrange files

--- confirm_dqn_env.py
+++ confirm_dqn_env.py
@@ -2,14 +2,46 @@ from collections import deque
 import gym
 import numpy as np
 from PIL import Image
-from agents.choice_agent import ChoiceAgent
+from chainer import Chain
+import chainer.functions as F
+from chainer import Variable
+
+
+class ChainerAgent(Chain):
+
+    def __init__(self, action, other_action, size, epsilon=0.05, hidden=200):
+        self.action = action
+        self.other_action = other_action
+        self.width = size * size
+        self.epsilon = epsilon
+        self.hidden = hidden
+        super(ChainerAgent, self).__init__(
+            l1=F.Linear(self.width, self.hidden, wscale=np.sqrt(2)),
+            l2=F.Linear(self.hidden, 1, wscale=np.sqrt(2)),
+        )
+    
+    def __call__(self, state):
+        s = Variable(np.array([state]))
+        h = F.relu(self.l1(s))
+        v = self.l2(h)
+        p = F.sigmoid(v)
+        return p.data[0][0], h.data[-1]
+
+    def act(self, state):
+        if np.random.uniform() < self.epsilon:
+            action = self.action if np.random.randint(2) == 0 else self.other_action
+        else:
+            prob, hidden = self(state)
+            action = self.action if np.random.uniform() < prob else self.other_action
+
+        return action
 
 
 def main(episode_count):
     env = gym.make("Pong-v0")
     size = 80
     frame_merge = 4
-    agent = ChoiceAgent(2, 3, size)
+    agent = ChainerAgent(2, 3, size)
 
     for i in range(episode_count):
         observation = env.reset()
@@ -47,4 +79,4 @@ def cut_play_area(observation, size):
 
 
 if __name__ == "__main__":
-    main(episode_count=20)
+    main(episode_count=2)
