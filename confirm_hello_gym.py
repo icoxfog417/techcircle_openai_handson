9c894b3308b772f636f03bec448d1a47ef61e760
icoxfog417 <icoxfog417@yahoo.co.jp>
2016-11-16 09:50:13 +0900

    fix confirm program

--- confirm_hello_gym.py
+++ confirm_hello_gym.py
@@ -1,17 +1,16 @@
 import gym
-from agents.random_agent import RandomAgent
 
 
 def main(episode_count):
     env = gym.make('CartPole-v0')
-    agent = RandomAgent(env.action_space.n)
+
     for i in range(episode_count):
         observation = env.reset()  # initialize the environment
         done = False
         step = 0
         while not done:
             env.render()
-            action = agent.act(observation)
+            action = env.action_space.sample()
             next_observation, reward, done, info = env.step(action)
             if done:
                 print("Episode finished after {} timesteps".format(step + 1))
